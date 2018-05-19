65864b126486209066b9d15259980e31998b9035
Marcos Riso <mriso@intelitica.com>
2018-05-19 09:23:28 -0300

    Fixed error on checkbox filter

--- App/Views/Components/checkbox.blade.php
+++ App/Views/Components/checkbox.blade.php
@@ -1,10 +1,12 @@
 <div class="form-group">
     <div class="col-sm-offset-3 col-sm-9">
         <div class="checkbox">
-            <label for="{{ $field->Field }}">
-                <input type="checkbox" value="1" @if($info->readonly) readonly @endif @if($info->required) required @endif
-                id="{{ $field->Field }}" @if(isset($queryObj) && $queryObj->{$field->Field} == 1) checked @elseif($tableObj['action'] == 'insert') checked @endif name="{{ $field->Field }}" > {{ $info->display_name }}
-            </label>
+        <label for="{{ $field->Field }}">
+            <input type="checkbox" value="1" @if($info->readonly) readonly @endif @if($info->required) required @endif
+            id="{{ $field->Field }}" @if(isset($queryObj) && $queryObj->{$field->Field} == 1) checked 
+            @elseif(isset($tableObj['action']) && $tableObj['action'] == 'insert') checked 
+            @endif name="{{ $field->Field }}" > {{ $info->display_name }}
+        </label>
         </div>
     </div>
 </div>
